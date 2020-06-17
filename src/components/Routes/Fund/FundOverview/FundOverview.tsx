9e809860a6d8f7ffc287e6a59b8fe3d6572842b4
Erin Koen <erin.koen@gmail.com>
2020-06-17 15:27:07 -0400

    remove metrics components

--- src/components/Routes/Fund/FundOverview/FundOverview.tsx
+++ src/components/Routes/Fund/FundOverview/FundOverview.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { FundHoldings } from '../FundHoldings/FundHoldings';
-import { FundPolicies } from '../FundDiligence/FundPolicies/FundPolicies';
-import { FundContracts } from '../FundDiligence/FundContracts/FundContracts';
-import { FundFactSheet } from '../FundDiligence/FundFactSheet/FundFactSheet';
 import { NewFundPerformanceChart } from '../FundPerformanceChart/FundPerformanceChart';
 import { Grid, GridRow, GridCol } from '~/storybook/Grid/Grid';
 import { FundPerformanceTable } from '~/components/Routes/Fund/FundPerfomanceTable/FundPerformanceTable';
-import { FundPerformanceMetrics } from '../FundReturns/FundPerformanceMetrics';
 import { FundDiligence } from '../FundDiligence/FundDiligence';
 
 export interface FundOverviewProps {
@@ -20,12 +16,6 @@ export const FundOverview: React.FC<FundOverviewProps> = ({ address }) => (
         <NewFundPerformanceChart address={address} />
       </GridCol>
     </GridRow>
-    {/* <GridRow>
-      <GridCol xs={12} sm={12}>
-        <FundPerformanceMetrics address={address} />
-      </GridCol>
-    </GridRow> */}
-
     <GridRow>
       <GridCol>
         <FundPerformanceTable address={address} />
