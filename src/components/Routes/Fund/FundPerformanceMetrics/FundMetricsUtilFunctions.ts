ec549d715fec307a421a8e98031a0e79ac6158d8
Erin Koen <erin.koen@gmail.com>
2020-07-21 17:55:51 -0400

    remove duplicative hooks, index queries

--- src/components/Routes/Fund/FundPerformanceMetrics/FundMetricsUtilFunctions.ts
+++ src/components/Routes/Fund/FundPerformanceMetrics/FundMetricsUtilFunctions.ts
@@ -1,61 +1,8 @@
 import { useQuery } from 'react-query';
 import { BigNumber } from 'bignumber.js';
-import { findCorrectToTime } from '~/utils/priceServiceDates';
 import { endOfMonth, subMonths, addMonths } from 'date-fns';
 import { calculateReturn } from '~/utils/finance';
-
-export interface RangeTimelineItem {
-  timestamp: number;
-  rates: {
-    [symbol: string]: number;
-  };
-  holdings: {
-    [symbol: string]: number;
-  };
-  shares: number;
-  calculations: {
-    price: number;
-    gav: number;
-    nav: number;
-  };
-}
-
-interface DepthTimelineItem {
-  timestamp: number;
-  rates: {
-    [symbol: string]: number;
-  };
-  holdings: {
-    [symbol: string]: number;
-  };
-  shares: number;
-  calculations: {
-    price: number;
-    gav: number;
-    nav: number;
-  };
-}
-
-export interface MonthendTimelineItem {
-  timestamp: number;
-  rates: {
-    [symbol: string]: number;
-  };
-  holdings: {
-    [symbol: string]: number;
-  };
-  shares: number;
-  references: {
-    ethusd: number;
-    etheur: number;
-    ethbtc: number;
-  };
-  calculations: {
-    price: number;
-    gav: number;
-    nav: number;
-  };
-}
+import { MonthendTimelineItem } from '~/hooks/metricsService/useFetchFundPricesByMonthEnd';
 
 export interface DisplayData {
   label?: string;
@@ -71,118 +18,7 @@ export interface MonthlyReturnData {
   BITWISE10?: DisplayData[];
 }
 
-export type Depth = '1y' | '6m' | '3m' | '1m' | '1w' | '1d';
-
-async function fetchFundPricesByDepth(key: string, fund: string, depth: Depth) {
-  const api = process.env.MELON_METRICS_API;
-  const url = `${api}/api/depth/onchain?address=${fund}&depth=${depth}`;
-  const response = await fetch(url)
-    .then((res) => res.json())
-    .catch((error) => console.log(error));
-  return response;
-}
-
-export function useFetchFundPricesByDepth(fund: string, depth: Depth) {
-  const address = fund.toLowerCase();
-  const key = 'pricesByDepth';
-  return useQuery([key, address, depth], fetchFundPricesByDepth, {
-    refetchOnWindowFocus: false,
-  });
-}
-
-async function fetchMonthlyFundPrices(key: string, address: string) {
-  const url = process.env.MELON_METRICS_API;
-  const queryAddress = `${url}/api/monthend?address=${address}`;
-  const response = await fetch(queryAddress)
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-  return response;
-}
-
-export function useFetchMonthlyFundPrices(fund: string) {
-  const address = fund.toLowerCase();
-  const key = 'monthlyPrice';
-  return useQuery([key, address], fetchMonthlyFundPrices, {
-    refetchOnWindowFocus: false,
-  });
-}
-
-async function fetchFundPricesByDate(key: string, address: string, from: number, to: number) {
-  const url = process.env.MELON_METRICS_API;
-  const queryAddress = `${url}/api/range?address=${address}&from=${from}&to=${to}`;
-  const response = await fetch(queryAddress)
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-  return response;
-}
-
-export function useFetchFundPricesByDate(fund: string, from: number, to: number) {
-  const address = fund.toLowerCase();
-  const key = 'rangeOfPrices';
-  return useQuery([key, address, from, to], fetchFundPricesByDate, {
-    refetchOnWindowFocus: false,
-  });
-}
-
-async function fetchIndexPrices(key: string, startDate: string, endDate: string) {
-  const apiKey = '007383bc-d3b7-4249-9a0d-b3a1d17113d9';
-  const queryAddress = `https://api.bitwiseinvestments.com/api/v1/indexes/BITWISE10/history?apiKey=${apiKey}&start=${startDate}&end=${endDate}`;
-  const response = await fetch(queryAddress)
-    .then((response) => response.json())
-    .catch((error) => console.log(error));
-  const prices = response.map((item: number[]) => new BigNumber(item[1]));
-  return prices as BigNumber[];
-}
-
-export async function fetchMultipleIndexPrices(dates: [Date, Date][]) {
-  const prices = await Promise.all(
-    dates.map((item) => {
-      return fetchIndexPrices('blank', item[0].toISOString(), item[1].toISOString());
-    })
-  );
-  return prices;
-}
-
-export function useFetchIndexPrices(startDate: string, endDate: string) {
-  return useQuery([startDate + endDate, startDate, endDate], fetchIndexPrices, {
-    refetchOnWindowFocus: false,
-  });
-}
-
-async function fetchReferencePricesByDate(key: string, date: number) {
-  const url = process.env.MELON_RATES_API;
-  const btcQueryAddress = `${url}/api/day-average?base=ETH&quote=BTC&day=${date}`;
-  const usdQueryAddress = `${url}/api/day-average?base=ETH&quote=USD&day=${date}`;
-  const eurQueryAddress = `${url}/api/day-average?base=ETH&quote=EUR&day=${date}`;
-
-  try {
-    const [btcResponse, usdResponse, eurResponse] = await Promise.all([
-      fetch(btcQueryAddress).then((response) => response.json()),
-      fetch(usdQueryAddress).then((response) => response.json()),
-      fetch(eurQueryAddress).then((response) => response.json()),
-    ]);
-
-    return {
-      ethbtc: btcResponse.data.rate,
-      ethusd: usdResponse.data.rate,
-      etheur: eurResponse.data.rate,
-    };
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
-
-export function useFetchReferencePricesByDate(date: Date) {
-  const day = findCorrectToTime(date);
-  const key = 'referencePricesByDate';
-  return useQuery([key, day], fetchReferencePricesByDate, {
-    refetchOnWindowFocus: false,
-  });
-}
-
 /**
- *
  * @param monthlyReturnData the data returned from a monthend call to our metrics service
  * @param dayZeroFx an object with the ethusd, etheur, and ethbtc VWAP prices from the day of the fund's inception
  * @param indexReturnData an array of [startOfMonthPrice, endOfMonthPRice] prices generated from the call to the bitwise api above
@@ -200,7 +36,6 @@ export function monthlyReturnsFromTimeline(
     ethusd: number;
     etheur: number;
   },
-
   today?: Date,
   activeMonths?: number,
   monthsBeforeFund?: number,
