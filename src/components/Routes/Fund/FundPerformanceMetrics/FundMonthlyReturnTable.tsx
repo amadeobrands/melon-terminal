a0778765108f374d4c326d078e55c14fefca5329
Erin Koen <erin.koen@gmail.com>
2020-07-21 16:24:20 -0400

    add box to feb error message, remove commented finance functions

--- src/components/Routes/Fund/FundPerformanceMetrics/FundMonthlyReturnTable.tsx
+++ src/components/Routes/Fund/FundPerformanceMetrics/FundMonthlyReturnTable.tsx
@@ -186,82 +186,16 @@ export const FundMonthlyReturnTable: React.FC<MonthlyReturnTableProps> = ({ addr
           </ScrollableTable>
         </>
       ) : (
-        <NotificationBar kind="error">
-          <NotificationContent>
-            There is a known issue where funds created in February 2020 cannot query their monthly returns. This is a
-            temporary error message while we fix this issue.
-          </NotificationContent>
-        </NotificationBar>
+        <>
+          <SectionTitle>Monthly Returns</SectionTitle>
+          <NotificationBar kind="error">
+            <NotificationContent>
+              There is a known issue where funds created in February 2020 cannot query their monthly returns. This is a
+              temporary error message while we fix this issue.
+            </NotificationContent>
+          </NotificationBar>
+        </>
       )}
     </Block>
   );
 };
-
-/**
- * RETURN MATH FOR VOLATILITY AND VAR CALS
- */
-// // indexData is an array of bignumbers, every index price since the date that's passed
-// // indexDailyReturns is an array of bignumbers, with index 0 being the return on day 0 and index(len-1) the return yesterday
-// const indexDailyReturns = indexData ? calculateDailyLogReturns(indexData) : [new BigNumber('NaN')];
-// // periodIndexVol takes the daily returns as a parameter and returns a bignumber == the volatility of those returns
-// const periodIndexVol = indexData
-//   ? calculateVolatility(calculateStdDev(indexDailyReturns), indexDailyReturns.length)
-//   : new BigNumber('Nan');
-// // periodIndexReturn shows the return if one were to hold an index for the time period
-// const periodIndexReturn = indexData ? calculatePeriodReturns(indexData) : new BigNumber('Nan');
-// // periodIndexVar takes the daily returns as a parameter and returns an object where lowZ == 95% CI and highZ = 99% CI
-// const periodIndexVAR = indexDailyReturns && calculateVAR(indexDailyReturns);
-
-// // fundData.data is an array of price objects with onchain and offchain prices as well as fund holdings data
-// // onChainPriceUpdates is an array of BigNumbers with the duplicate onchain prices stripped out (would otherwise remain constant for a day)
-// const onChainPriceUpdates = fundData && stripDuplicateOnchainPrices(fundData.data);
-// // onChainDailyReturns is an array of BigNumbers showing the inter-day returns starting with day 1's return  and ending with yesterday's
-// const onChainDailyReturns = onChainPriceUpdates
-//   ? calculateDailyLogReturns(onChainPriceUpdates)
-//   : [new BigNumber('Nan')];
-// // periodFundVol takes the onChainDailyReturns as a parameter and returns a bignumber === the volatility of those returns
-// const periodFundVol =
-//   onChainDailyReturns && calculateVolatility(calculateStdDev(onChainDailyReturns), onChainDailyReturns.length);
-// // periodFundReturn takes the onChainPriceUpdates as a parameter and returns a bignumner
-// const periodFundReturn = onChainPriceUpdates ? calculatePeriodReturns(onChainPriceUpdates) : new BigNumber('Nan');
-// // periodFundVar takes the daily returns as a parameter and returns an object where lowZ = 95% CI and highZ = 9% CI
-// const periodFundVAR = onChainDailyReturns && calculateVAR(onChainDailyReturns);
-
-{
-  /* <DictionaryEntry>
-        <DictionaryLabel>{depth} Fund Volatility </DictionaryLabel>
-        <DictionaryData textAlign={'right'}>
-          <FormattedNumber decimals={2} value={periodFundVol} suffix={'%'} />
-        </DictionaryData>
-      </DictionaryEntry>
-      <DictionaryEntry>
-        <DictionaryLabel>{depth} BitWise 10 Index Vol</DictionaryLabel>
-        <DictionaryData textAlign={'right'}>
-          <FormattedNumber decimals={2} value={periodIndexVol} suffix={'%'} />
-        </DictionaryData>
-      </DictionaryEntry>
-      <DictionaryEntry>
-        <DictionaryLabel>{depth} Fund Return</DictionaryLabel>
-        <DictionaryData textAlign={'right'}>
-          <FormattedNumber decimals={2} value={periodFundReturn} colorize={true} suffix={'%'} />
-        </DictionaryData>
-      </DictionaryEntry>
-      <DictionaryEntry>
-        <DictionaryLabel>{depth} BitWise 10 Index Return</DictionaryLabel>
-        <DictionaryData textAlign={'right'}>
-          <FormattedNumber decimals={2} value={periodIndexReturn} colorize={true} suffix={'%'} />
-        </DictionaryData>
-      </DictionaryEntry>
-      <DictionaryEntry>
-        <DictionaryLabel>{depth} Fund VAR</DictionaryLabel>
-        <DictionaryData textAlign={'right'}>
-          <FormattedNumber decimals={2} value={periodFundVAR?.lowZ} colorize={false} suffix={'%'} />
-        </DictionaryData>
-      </DictionaryEntry>
-      <DictionaryEntry>
-        <DictionaryLabel>{depth} BitWise 10 Index VAR</DictionaryLabel>
-        <DictionaryData textAlign={'right'}>
-          <FormattedNumber decimals={2} value={periodIndexVAR?.lowZ} colorize={false} suffix={'%'} />
-        </DictionaryData>
-      </DictionaryEntry> */
-}
